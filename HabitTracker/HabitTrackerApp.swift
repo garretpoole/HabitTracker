f752ce7af8caca258cfc99a435c506f2be77bd13
Garret Poole <garretpoole@users.noreply.github.com>
2022-03-24 14:59:16 -0700

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Garret Poole on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
